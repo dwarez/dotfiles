04c249f986ca6eea97196aa79d74a81bafacacfa
DWarez <ldwarezl@gmail.com>
2026-04-25 11:23:22 +0200

    Revert nvim to kanagawa-dragon (keep matugen template to silence noctalia warning)

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,30 @@
 return {
-  "LazyVim/LazyVim",
-  opts = {
-    colorscheme = function() end,
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      compile = true,
+      undercurl = true,
+      commentStyle = { italic = true },
+      keywordStyle = { italic = true },
+      statementStyle = { bold = true },
+      terminalColors = true,
+      theme = "dragon",
+      background = {
+        dark = "dragon",
+        light = "lotus",
+      },
+    },
+    config = function(_, opts)
+      require("kanagawa").setup(opts)
+      vim.cmd("colorscheme kanagawa-dragon")
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa-dragon",
+    },
   },
 }
